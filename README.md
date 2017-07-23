8c02185764ea649ad3d5fdf08cb21b7744a7479b
Hendrik Wallbaum <mail@hendrikwallbaum.de>
2017-07-23 19:41:55 +0200

    :sparkle: display blog posts.

--- README.md
+++ README.md
@@ -5,3 +5,10 @@ My personal page available at: http://hendrikwallbaum.de/
 ## Dev
 
 Build on Stylus. Run `npm start` to get a live preview and stylus compiled.
+
+## Roadmap
+
+* [ ] Better extracts from posts.
+* [ ] Social links
+* [ ] Footer
+* [ ] Contact something
--- js/script.js
+++ js/script.js
@@ -4,12 +4,35 @@
 //from the actual blog or a local file.
 const blogUrl = (location.hostname === '127.0.0.1' || location.hostname === 'localhost') ? 'php/blog.json' : 'php/getBlogPosts.php'
 
-get(blogUrl, (posts) => {
-    posts = JSON.parse(posts)
-    console.log(posts)
+//MAximum number of blogposts to display.
+const maxBlogPosts = 6
 
-    
+get(blogUrl, (rss) => {
+    rss = JSON.parse(rss)
+    console.log(rss)
+
+    const postsHtml = rss.entry
+        .slice(0, maxBlogPosts)
+        .reduce((html, post) => {
+            return html + `
+                <section class="portfolio">
+                    <h2 class="portfolio__title"><a href="${post.id}">${post.title}</a></h2>
+                    ${generatePostSummary(post, rss.id)}
+                    <p>
+                        <a href="${post.id}">&hellip; weiter</a>
+                    </p>
+                </section>
+            `
+        }, '')
+    const wrapper = document.querySelector('#blogPostsWrapper')
+    wrapper.innerHTML = postsHtml
 })
+
+function generatePostSummary(post, baseUrl) {
+    let summary = post.summary
+    summary = summary.replace(/src="\//g, `src="${baseUrl}`)
+    return summary
+}
 
 
 //Wrap a http-get-request in an easier to use function
